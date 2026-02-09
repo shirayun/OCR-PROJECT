f33c74dd1d8e6f74d15a325ebc9ad3ae1e772eda
Shira Yungraise <giti9089039@gmail.com>
2026-02-09 16:27:15 +0200

    G

--- Fronted/sr-client/src/app/app.routes.ts
+++ Fronted/sr-client/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'camera' },
-  { path: 'camera', loadComponent: () => import('./camera.component').then(m => m.CameraComponent) }
+  { path: '', loadComponent: () => import('./camera.component').then(m => m.CameraComponent) }
 ];
